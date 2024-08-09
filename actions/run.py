cf5c7e79d6bfeef96d4bff9774662b85e8b0caa3
Mario Kakoulli <mario.kakoulli78@thehutgroup.com>
2024-08-09 12:13:51 +0100

    sync with upstream

--- actions/run.py
+++ actions/run.py
@@ -12,15 +12,18 @@ class NetboxHTTPAction(NetboxBaseAction):
         """Base run action
         """
 
-        if http_verb in ('get', 'post') and kwargs.get('id', False) and get_detail_route_eligible:
-            # modify the `endpoint_uri` to use the detail route
-            endpoint_uri = f"{endpoint_uri}{str(kwargs.pop('id'))}/"
-            self.logger.debug(f'endpoint_uri transformed to {endpoint_uri} because id was passed')
-
-
         if http_verb == 'get':
+            if kwargs.get('id', False) and get_detail_route_eligible:
+                # modify the `endpoint_uri` to use the detail route
+                endpoint_uri = '{}{}/'.format(endpoint_uri,
+                                              str(kwargs.pop('id')))
+                self.logger.debug(
+                    'endpoint_uri transformed to {} because id was passed'.format(
+                        endpoint_uri)
+                )
+
             if kwargs.get('save_in_key_store') and not kwargs.get('save_in_key_store_key_name'):
-                return False, 'save_in_key_store_key_name MUST be used with save_in_key_store!'
+                return (False, 'save_in_key_store_key_name MUST be used with save_in_key_store!')
 
             result = self.make_request(endpoint_uri, http_verb, **kwargs)
 
@@ -31,7 +34,8 @@ class NetboxHTTPAction(NetboxBaseAction):
                 client.keys.update(KeyValuePair(name=key_name, value=json.dumps(result),
                                                 ttl=kwargs['save_in_key_store_ttl']))
 
-                return True, f"Result stored in st2 key {key_name}"
+                return (True, "Result stored in st2 key {}".format(key_name))
+
         else:
             result = self.make_request(endpoint_uri, http_verb, **kwargs)
 
